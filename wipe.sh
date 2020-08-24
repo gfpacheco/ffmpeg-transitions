5529d973cc438292e82a88d858003f8279802993
Guilherme Pacheco <guilherme.f.pacheco@gmail.com>
2020-08-24 11:45:09 -0300

    Fix wipe not transitioning first pixel column

--- wipe.sh
+++ wipe.sh
@@ -5,5 +5,5 @@ ffmpeg \
   -filter_complex "\
     [0:v]setdar=16/9,setpts=PTS-STARTPTS[0v]; \
     [1:v]setdar=16/9,setpts=PTS-STARTPTS+3.75/TB[1v]; \
-    [0v][1v]blend=all_expr='if(lte(min(1,max((T-3.75)/0.25,0)),1-X/W),A,B)'[v] \
+    [0v][1v]blend=all_expr='if(lt(min(1,max((T-3.75)/0.25,0)),1-X/W),A,B)'[v] \
   " -map [v] output.mp4
